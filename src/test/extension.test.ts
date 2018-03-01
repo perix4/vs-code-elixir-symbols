b433c19e2aac5364f64549fd32d1b1c915d1e4d5
Petar Kujundzic <petar.kujundzic@limun-studio.hr>
2018-03-01 10:08:13 +0100

    Fixes finding functions without parenthesis

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,6 +5,14 @@ import * as myExtension from '../extension';
 import ElixirDocumentSymbolProvider from '../elixir_document_symbol_provider'
 
 suite("Extension Tests", () => {
+    test("Regex to find functions without parenthesis", () => {
+        const provider = new ElixirDocumentSymbolProvider()
+
+        const normal_function = "def function_name do"
+        assert.equal("", provider.matchFunction(normal_function)[1])
+        assert.equal("function_name", provider.matchFunction(normal_function)[2])
+
+    });
     test("Regex to find functions", () => {
         const provider = new ElixirDocumentSymbolProvider()
 
@@ -19,6 +27,14 @@ suite("Extension Tests", () => {
         const oneliner_function = "def function_name(param1, param2), do: something_else"
         assert.equal("", provider.matchFunction(oneliner_function)[1])
         assert.equal("function_name(param1, param2)", provider.matchFunction(oneliner_function)[2])
+    });
+    test("Regex to find private functions without parenthesis", () => {
+        const provider = new ElixirDocumentSymbolProvider()
+
+        const normal_function = "defp function_name do"
+        assert.equal("p", provider.matchFunction(normal_function)[1])
+        assert.equal("function_name", provider.matchFunction(normal_function)[2])
+
     });
     test("Regex to find private functions", () => {
         const provider = new ElixirDocumentSymbolProvider()
